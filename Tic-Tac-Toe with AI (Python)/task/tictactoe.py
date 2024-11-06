d002fc4adc531cc2cc8ed27383f3a8ce6e483b98
Moha <mimohadinho@gmail.com>
2024-11-06 16:00:45 +0100

    Stage 2/5:Easy does it

--- Tic-Tac-Toe with AI (Python)/task/tictactoe.py
+++ Tic-Tac-Toe with AI (Python)/task/tictactoe.py
@@ -1,5 +1,5 @@
 # write your code here
-
+import random
 board = []
 def initialize_board(board_p=''):
     """Initialize board"""
@@ -7,7 +7,7 @@ def initialize_board(board_p=''):
         for i in range(3):
             temp = []
             for j in range(3):
-                temp.append('_')
+                temp.append(' ')
             board.append(temp)
     else:
         sp = [*board_p]
@@ -44,7 +44,7 @@ def check_space(coordinate):
             sp = coordinate.split()
             x = int(sp[0])
             y = int(sp[1])
-            if board[x - 1][y - 1] != '_':
+            if board[x - 1][y - 1] != ' ':
                 print('This cell is occupied! Choose another one!')
                 coordinate = input('Enter the coordinates: ')
                 continue
@@ -69,16 +69,24 @@ def place_move(move):
     x = int(move[0]) - 1
     y = int(move[1]) - 1
 
-    join_board = [item for innerlist in board for item in innerlist] # join board to one list and count the elements
-    x_count = join_board.count('X') # count x
-    y_count = join_board.count('O') # count y
+    # join_board = [item for innerlist in board for item in innerlist] # join board to one list and count the elements
+    # x_count = join_board.count('X') # count x
+    # y_count = join_board.count('O') # count y
 
-    if x_count > y_count:
+    board[x][y] = 'X'
+
+def computer_move():
+    while True:
+        x = random.randint(0, 2)
+        y = random.randint(0, 2)
+
+        if board[x][y] != ' ':
+            continue
+
         board[x][y] = 'O'
-    elif y_count > x_count:
-        board[x][y] = 'X'
-    else:
-        board[x][y] = 'X'
+        break
+
+    print('Making move level "easy"')
 
 def winner_state():
     """Check the state of the game and print the winner"""
@@ -95,31 +103,39 @@ def winner_state():
 
     # Check each line for a winner
     for line in winning_lines:
-        if line.count(line[0]) == 3 and line[0] != '_':
+        if line.count(line[0]) == 3 and line[0] != ' ':
             print(f'{line[0]} wins')
             return True
 
     # Check for draw
-    if all(cell != '_' for row in board for cell in row):
+    if all(cell != ' ' for row in board for cell in row):
         print('Draw')
         return True
 
-    print('Game not finished')
+    #print('Game not finished')
     return False
 
 def game():
     """Game starts when user puts in the board"""
     start = True
-    while start:
-        input_board = input('Enter the cells: ')
-        if not board_validity(input_board):
-            continue
-        initialize_board(input_board) # Initialize board
-        game_state() # Print the state of the game
+    index = 0 # to take turns
 
-        coordinates = input('Enter the coordinates: ')
-        coord = check_space(coordinates) # check space if valid
-        place_move(coord)
+    initialize_board()  # Initialize board
+    game_state()  # Print the state of the game
+
+    while start:
+        # input_board = input('Enter the cells: ')
+        # if not board_validity(input_board):
+           # continue
+        if index == 0:
+            coordinates = input('Enter the coordinates: ')
+            coord = check_space(coordinates) # check space if valid
+            place_move(coord)
+            index = 1
+        else:
+            computer_move()
+            index = 0
+
         game_state()
         stop = winner_state() # check state after we place a move
         if stop:
